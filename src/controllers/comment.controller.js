b83cf7e55f15af0944ce3851b8154b8ee49aa17a
sahin <sahindafer1919@gamil.com>
2025-03-07 03:27:51 +0530

    Fix some file naming issue

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
+import {Comment} from "../models/comment.models.js"
 import { Video } from "../models/video.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
